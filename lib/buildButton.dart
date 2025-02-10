353466166b45d90beb93f22e97b2597863fa15c4
Md Takiul Islam Sumon <Takiulislam20@gmail.com>
2025-02-11 00:30:46 +0600

    Button Done

---
+++ lib/buildButton.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class buildButton extends StatelessWidget {
+  String text;
+  Color? color;
+  VoidCallback onClick;
+  buildButton({
+    super.key,
+    required this.text,
+    required this.onClick,this.color
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          onPressed: onClick,
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          style: ElevatedButton.styleFrom(
+              backgroundColor: color ?? Colors.grey[850],
+              padding: const EdgeInsets.all(24),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15))),
+        ),
+      ),
+    );
+  }
+}
